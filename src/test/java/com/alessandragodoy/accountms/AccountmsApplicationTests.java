28035d2bb203bb5a0e093751ed3ca2cfb9f5ed27
abengl <126703861+abengl@users.noreply.github.com>
2024-11-12 15:17:54 -0500

    :tada: NTG-23 init: base files for account microservice

---
+++ src/test/java/com/alessandragodoy/accountms/AccountmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alessandragodoy.accountms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
